835d9e347708e43051fd5e1b57c972d6c88309d7
Hans-Peter Buniat <hpbuniat@gmail.com>
2011-06-17 22:43:51 +0200

    added src-testsuite to allow test-driven creation of new rules, added rule to detect duplicate selectors

---
+++ src/rules/duplicate-selectors.js
@@ -0,0 +1,47 @@
+/*
+ * Rule: Selectors should be unique
+ */
+CSSLint.addRule({
+
+    //rule information
+    id: "duplicate-selectors",
+    name: "Duplicate Selectors",
+    desc: "Selectors should be used only once.",
+    browsers: "All",
+
+    //initialization
+    init: function(parser, reporter){
+        var rule = this;
+
+        var selectorStack =  {};
+        parser.addListener("startrule", function(event){
+            var selectors = event.selectors,
+                selector,
+                part,
+                modifier,
+                i, j, k;
+
+            for (i=0; i < selectors.length; i++){
+                selector = selectors[i];
+                if (typeof selectorStack[selector.text] === 'undefined') {
+                    selectorStack[selector.text] = 1;
+                }
+                else {
+                    selectorStack[selector.text]++;
+                }
+            }
+        });
+
+        //report the results
+        parser.addListener("endstylesheet", function(event){
+            reporter.stat("duplicate-selectors", selectorStack);
+            for (i in selectorStack) {
+                if (selectorStack[i] > 1) {
+                    reporter.rollupWarn("Selectors should be used only once, " + i + " was used " + selectorStack[i] + " times." , rule);
+                }
+            }
+        });
+        delete selectorStack;
+    }
+
+});
